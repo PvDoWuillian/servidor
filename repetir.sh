89f8b04e6c03109c64b76ef59525ee07a6399da0
Wuillian <r83238821@gmail.com>
2026-08-14 02:08:43 +0000

    Automação de 12 horas com auto-inicialização

--- repetir.sh
+++ repetir.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 contador=1
 while [ $contador -le 350 ]; do
-  echo "salvo (Minuto $contador)"
+  echo "suapalavra (Minuto $contador)"
   sleep 60
   contador=$((contador+1))
 done
+
+# --- COMANDO PARA REINICIAR SOZINHO (MAIS 6 HORAS) ---
+GITHUB_USER="PvDoWuillian"
+REPO_NAME="servidor"
+WORKFLOW_ID="rotina.yml"
+
+echo "Iniciando a segunda metade das 12 horas..."
+
+curl -X POST \
+  -H "Authorization: token ${GITHUB_TOKEN}" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://github.com{GITHUB_USER}/${REPO_NAME}/actions/workflows/${WORKFLOW_ID}/dispatches \
+  -d '{"ref":"main"}'
